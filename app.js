48d85dd8d9fabca7a133409e073685570a192d2b
Danylo Altukhov <dan.97198@gmail.com>
2024-04-02 18:40:11 +0200

    add new controller and upd swagger
    Add new controller for route calendar/today, upd swagger info

--- app.js
+++ app.js
@@ -7,14 +7,13 @@ import "./db.js";
 import authRouter from "./routes/authRouter.js";
 import waterRouter from "./routes/waterRouter.js";
 import usersRouter from "./routes/usersRouter.js";
-
+import calendarRouter from "./routes/calendarRouter.js";
 
 // import swaggerDocument from "./swagger.json" assert { type: "json" };
 import { createRequire } from "node:module";
 import swaggerUi from "swagger-ui-express";
 const require = createRequire(import.meta.url);
 const swaggerDocument = require("./swagger.json");
-
 
 const app = express();
 
@@ -26,17 +25,17 @@ app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/water", waterRouter);
-// app.use("/api/calendar", calendarRouter);
+app.use("/api/calendar", calendarRouter);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use((_, res) => {
-	res.status(404).json({ message: "Route not found" });
+  res.status(404).json({ message: "Route not found" });
 });
 
 app.use((err, req, res, next) => {
-	const { status = 500, message = "Server error" } = err;
-	res.status(status).json({ message });
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
 });
 
 app.listen(3000, () => {
-	console.log("Server is running. Use our API on port: 3000");
+  console.log("Server is running. Use our API on port: 3000");
 });
--- routes/calendarRouter.js
+++ routes/calendarRouter.js
@@ -1,11 +1,20 @@
 import express from "express";
 import validateBody from "../helpers/validateBody.js";
-// import {... } from "../controllers/calendarControllers.js";
-// import { ... } from "../schemas/calendarSchemas.js";
+import auth from "../middleware/auth.js";
+import calendarControllers from "../controllers/calendarToday.js";
+import calendarMonth from "../controllers/calendarMonthController.js";
+import calendarMonthSchema from "../schemas/calendarSchema.js";
 
 const calendarRouter = express.Router();
 
 // роути
 // наприклад: calendarRouter.post("/today", validateBody(userSchema), consumptionToday);
+calendarRouter.get("/today", auth, calendarControllers.countWaterUseToday);
+calendarRouter.get(
+  "/month",
+  auth,
+  validateBody(calendarMonthSchema),
+  calendarMonth
+);
 
 export default calendarRouter;
